1325120be383c40a516601194d03668a539392ab
lesca <lesca@users.noreply.github.com>
2021-05-18 13:43:12 +0800

    Update and rename tarEncryption to dirEncrypt.sh

--- dirEncrypt.sh
+++ dirEncrypt.sh
@@ -1,10 +1,12 @@
 # scripts to easily run gpgtar to encrypt / decrypt directories 
 
-# passfile 
+# passfile
 # a file containing **one-line** passphrase
 # put this file at the working directory where you run the command
 
-# openssl - better performace
+# openssl version - better performace tan gpg
+
+# encrypt a specified directory to tar file and split into 2GB in size
 sslen() { [ "$2" = "" ] && des=encrypted || des=$2 ; mkdir -p $des ; tar -c $1 | openssl enc -aes-256-cbc -pass file:passfile | split -a3 -d -b 2G - $des/`basename $1`.part ; }
 sslde() { [ "$2" = "" ] && des=decrypted || des=$2 ; mkdir -p $des ; cat $1* | openssl enc -aes-256-cbc -d -pass file:passfile | tar -xC $des ; }
 
@@ -16,7 +18,7 @@ dirde() { [ "$2" = "" ] && des=decrypted || des=$2 ; mkdir -p $des ; find ./$1/ 
 diren() { [ "$2" = "" ] && des=encrypted || des=$2 ; mkdir -p $des ; find ./$1/ | while read f ; do ([ -f "$f" ] && [ ! -f "$des/$f" ] && echo $f && openssl enc -aes-256-cbc -pass file:passfile -in "$f" > "$des/$f" 2>/dev/null ) || ([ -d "$f" ] && mkdir -p "$des/$f") ; done }
 dirde() { [ "$2" = "" ] && des=decrypted || des=$2 ; mkdir -p $des ; find ./$1/ | while read f ; do ([ -f "$f" ] && [ ! -f "$des/$f" ] && echo $f && openssl enc -d -aes-256-cbc -pass file:passfile -in "$f" > "$des/$f" 2>/dev/null ) || ([ -d "$f" ] && mkdir -p "$des/$f") ; done }
 
-# gpg - lower performance
+# gpg version - lower performance (not recommended, for refernece only)
 gpgen() { mkdir encrypted ; tar -c $1 | gpg -c --passphrase-file=passfile --batch -o - | split -a3 -d -b 2G - encrypted/`basename $1`.part ; }
 gpgde() { mkdir decrypted ; cat $1* | gpg -d --passphrase-file=passfile --batch -o - | tar -xC decrypted ; }
 
