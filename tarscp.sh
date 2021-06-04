da98a863caee65a023fcda2c688aec38b034fc8b
lesca <lesca@users.noreply.github.com>
2021-06-04 10:29:02 +0800

    Update tarscp.sh

--- tarscp.sh
+++ tarscp.sh
@@ -1,4 +1,3 @@
 # Usage:
 # tarcp src ip:dest/
-tarcp() { [ "$2" = "" ] && echo tarcp src ip:dest && return || src=$1 && ip=$(echo $2 | cut -d: -f1) && dest=$(echo $2 | cut -d: -f2) ; echo $ip:$dest ; tar -cvSf - $src --totals=USR1 --checkpoint=100000 --checkpoint-action=echo="#%u: %T" |
-ssh -T -c aes128-gcm@openssh.com -o Compression=no -x $ip "(mkdir -p $dest ; tar -xf - -C $dest)" ; }
+tarcp() { [ "$2" = "" ] && echo tarcp src ip:dest && return || src=$1 && ip=$(echo $2 | cut -d: -f1) && dest=$(echo $2 | cut -d: -f2) ; echo $ip:$dest ; tar -cvSf - $src --totals=USR1 --checkpoint=100000 --checkpoint-action=echo="#%u: %T" | ssh -T -c aes128-gcm@openssh.com -o Compression=no -x $ip "(mkdir -p $dest ; tar -xf - -C $dest)" ; }
