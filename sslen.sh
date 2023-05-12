c0319c485e3524df5f83445d6d86c3365806038d
lesca <lesca@users.noreply.github.com>
2023-05-12 15:03:19 +0800

    split into 1GB
    support qnap as well

--- sslen.sh
+++ sslen.sh
@@ -4,7 +4,7 @@
 # put this file at the $HOME directory 
 
 # encrypt a specified directory to tar file and split into 2GB in size
-sslen() { [ "$2" = "" ] && des=encrypted || des=$2 ; mkdir -p $des ; tar --checkpoint=100000 --checkpoint-action=echo="#%u: %T" -c $1 | openssl enc -aes-256-cbc -pass file:$HOME/.passfile | split -a3 -d -b 2G - $des/`basename $1`.part ; }
+sslen() { [ "$2" = "" ] && des=encrypted || des=$2 ; mkdir -p $des ; tar --checkpoint=100000 --checkpoint-action=echo="#%u: %T" -c $1 | openssl enc -aes-256-cbc -pass file:$HOME/.passfile | split -a4 -d -b 1073741824 - $des/`basename $1`.part ; }
 sslde() { [ "$2" = "" ] && des=decrypted || des=$2 ; mkdir -p $des ; cat $1* | openssl enc -aes-256-cbc -d -pass file:$HOME/.passfile | tar --checkpoint=100000 --checkpoint-action=echo="#%u: %T" -xC $des ; }
 
 # recursively encrypt / decrypt a directory
